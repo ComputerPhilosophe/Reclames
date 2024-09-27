61b94ab19b4b255c3279a5176f4aada952aebb04
Caio Brun De Oliveira <20211IMI003@cefetes.br>
2024-09-27 08:01:43 -0300

    nomes alterados, criacao do db comecando

--- routes/morador_routes.py
+++ routes/morador_routes.py
@@ -6,10 +6,10 @@ from util.templates import obter_jinja_templates
 router = APIRouter(prefix="/morador")
 templates = obter_jinja_templates("templates/main")
 
-@router.get("/alterar_perfil_morador", response_class=HTMLResponse)
+@router.get("/alterar_perfil_morador_exemplo", response_class=HTMLResponse)
 async def get_root(request: Request):
-    return templates.TemplateResponse("pages/alterar_perfil_morador.html", {"request": request})
+    return templates.TemplateResponse("pages/alterar_perfil_morador_exemplo.html", {"request": request})
 
-@router.get("/perfil_morador", response_class=HTMLResponse)
+@router.get("/perfil_morador_exemplo", response_class=HTMLResponse)
 async def get_root(request: Request):
-    return templates.TemplateResponse("pages/perfil_morador.html", {"request": request})
+    return templates.TemplateResponse("pages/perfil_morador_exemplo.html", {"request": request})
--- routes/patrocinador_routes.py
+++ routes/patrocinador_routes.py
@@ -6,7 +6,7 @@ from util.templates import obter_jinja_templates
 router = APIRouter(prefix="/patrocinador")
 templates = obter_jinja_templates("templates/main")
 
-@router.get("/perfil_patrocinador", response_class=HTMLResponse)
+@router.get("/perfil_patrocinador_exemplo", response_class=HTMLResponse)
 async def get_root(request: Request):
     return templates.TemplateResponse("pages/perfil_patrocinador.html", {"request": request})
 
